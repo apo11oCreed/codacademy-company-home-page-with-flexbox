e571e7d41c16b09b58874a0c30e749aa6ee6689c
nc <nelson.ncdesigns@gmail.com>
2022-04-01 13:54:05 -0400

    Additional zones built with static assets

--- src/index.js
+++ src/index.js
@@ -5,31 +5,49 @@ import $ from "jquery";
 var jq = $.noConflict();
 (function($){
     let zoneImages=[],
-    ids=[];
+    ids=[],
+    bestSellingStylesImgs='/360/360';
+
     $(function() {
         zoneImages=$('[class*="zone"] img');
 
         $(zoneImages).each(function(item){
 
-            const randomNo=Math.floor(Math.random() * 10);
+            const randomNo=getRandomNumber(ids,zoneImages.length),
+            parent=$(zoneImages[item]).parents('[class*="zone"]');
 
-            for(let i=0;i<ids.length;i++ ){
-
-                if(randomNo==ids[i]){
-
-                    randomNo=Math.floor(Math.random() * 10);
-
-                    ids.push(randomNo);
-
-                } else{
-
-                    ids.push(randomNo);
-
-                }
+            if($(parent).hasClass('zone_5')){
+                $(zoneImages[item]).attr('src','https://picsum.photos/id/' + randomNo + bestSellingStylesImgs);
+            } else {
+                $(zoneImages[item]).attr('src','https://picsum.photos/id/' + randomNo + '/960/960');
             }
 
-            $(zoneImages[item]).attr('src','https://picsum.photos/id/' + randomNo + '/960/960')
-        })
+        });
+
     });
+
+    function getRandomNumber(array,qty){
+
+        let random=Math.floor(Math.random() * qty);
+
+        for(let i=0;i<array.length;i++ ){
+
+            if(random!==array[i]){
+
+                continue;
+
+            } else {
+
+                random=Math.floor(Math.random() * qty);
+
+            }
+
+        }
+
+        ids.push(random);
+
+        return random;
+
+    }
     
 })(jq);
